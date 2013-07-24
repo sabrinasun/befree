9d8610d55861d058712ee463b436bae392b0c92d
Long <julyzergcn1@gmail.com>
2013-07-24 10:46:41 +0800

    account summary should be login required

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 
 def to_template(template_name):
     return TemplateView.as_view(template_name=template_name)
@@ -10,11 +11,11 @@ urlpatterns = patterns('core.views',
     url(r'^to-reader/$', to_template('to-reader.html'), name='to_reader'),
     url(r'^shipping/$', to_template('shipping.html'), name='shipping'),
     url(r'^help/$', to_template('help.html'), name='help'),
-    url(r'^account/summary/$', to_template('account/summary.html'), name='account_summary'),
+    url(r'^account/summary/$', login_required(to_template('account/summary.html')), name='account_summary'),
     url(r'^account/orders/reading/$', to_template('account/orders/reading.html'), name='account_reading_orders'),
     url(r'^account/orders/giving/$', to_template('account/orders/giving.html'), name='account_giving_orders'),
     url(r'^account/material/$', to_template('account/material.html'), name='account_material'),
     url(r'^account/material/new/$', to_template('account/material_new.html'), name='account_material_new'),
     url(r'^account/material/new/existing/$', to_template('account/material_new_from_existing.html'), name='material_new_from_existing'),
-    
+
 )
