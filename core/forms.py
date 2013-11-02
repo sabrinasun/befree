e7ffee86a7ed1968666222937f862d575275a555
hello-color <kelvince@saggitdediannao.local>
2013-11-02 10:09:17 +0800

    change the show of `type` in the form

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from core.models import Material, GiverMaterial, Author, Publisher
 
 class MaterialForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(MaterialForm, self).__init__(*args, **kwargs)
+        self.fields['typ'].label = "Type"
     
     class Meta:
         model = Material
