a609e3a58c1e5d0af07a582784d4e3cb65c8da5e
hello-color <kelvince@saggitdediannao.local>
2013-11-04 22:24:59 +0800

    implement popup

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django import http
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponseNotFound, HttpResponse
+from django.utils.html import escape
+from django.forms.models import modelform_factory
+from django.db.models.loading import get_models, get_app, get_apps
 from django.core.urlresolvers import reverse_lazy
 from django.db import transaction
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.contrib import messages
@@ -180,16 +187,32 @@ def account_material_edit(request, material_id=None):
     return render(request, 'account/material_edit.html', context)
 
 @login_required
-def account_add_author(request):
-    next = request.REQUEST.get('next', '')
-    if request.method == 'POST':
-        form = AuthorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(next or 'account_material')
+def add_new_model(request, model_name):
+    if (model_name.lower() == model_name):
+            normal_model_name = model_name.capitalize()
     else:
-        form = AuthorForm()
-    return render(request, 'account/add_author.html', {'form': form, 'next': next})
+        normal_model_name = model_name
+    app_list = get_apps()
+    for app in app_list:
+        for model in get_models(app):
+            if model.__name__ == normal_model_name:
+                form = modelform_factory(model)
+            
+                if request.method == 'POST':
+                    form = form(request.POST)
+                    if form.is_valid():
+                        try:
+                            new_obj = form.save()
+                        except forms.ValidationError as error:
+                            new_obj = None
+                    
+                        if new_obj:
+                            return HttpResponse('<script type="text/javascript">opener.dismissAddAnotherPopup(window, "%s", "%s");</script>' % \
+                                                            (escape(new_obj._get_pk_val()), escape(new_obj)))
+                else:
+                    form = form()
+                page_context = {'form': form, 'field': normal_model_name}
+                return render_to_response('popup.html', page_context, context_instance=RequestContext(request))
 
 @login_required
 def account_add_publisher(request):
